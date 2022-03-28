0a3168357de6cfa87de11637d24c248662ae8102
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-03-28 19:51:42 +0200

    implement Alert component

--- packages/components/src/primer/index.js
+++ packages/components/src/primer/index.js
@@ -1,6 +1,11 @@
+import { Alert, alertVariant } from './Alert'
 import { Blankslate, blankslateVariant } from './Blankslate'
 
 export const primer = {
+  Alert: {
+    Alert,
+    alertVariant,
+  },
   Blankslate: {
     Blankslate,
     blankslateVariant,
