eaa933475dd8cb03fcafc201bbd380ca99bbff4a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-03-28 19:37:02 +0200

    install papillonbits library

--- packages/components/bumpstack.sh
+++ packages/components/bumpstack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-npm un -S @papillonbits/css && npm i -S @papillonbits/css
+npm un -S @papillonbits/css @papillonbits/library && npm i -S @papillonbits/css @papillonbits/library
 
 npm run clean-up && npm run install-packages && npm audit fix
