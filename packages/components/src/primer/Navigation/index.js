e4f251a5203038ad7c7ddc0b0230aa80d6570b0d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-03-29 16:43:23 +0200

    implement Navigation index

---
+++ packages/components/src/primer/Navigation/index.js
@@ -0,0 +1,12 @@
+import { Menu } from './Menu'
+import { TabNav, tabNavState } from './TabNav'
+import { UnderlineNav, underlineNavItemType, underlineNavAlignmentType } from './UnderlineNav'
+
+export const Navigation = {
+  Menu,
+  TabNav,
+  tabNavState,
+  UnderlineNav,
+  underlineNavItemType,
+  underlineNavAlignmentType,
+}
