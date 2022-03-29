8c79d8cf2ad4881f1dc8731b76b10983b71acc10
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-03-29 14:29:38 +0200

    extend Form index with Checkbox and Radio

--- packages/components/src/primer/Form/index.js
+++ packages/components/src/primer/Form/index.js
@@ -1,9 +1,19 @@
+import { Checkbox, checkboxState } from './Checkbox'
 import { Input, inputType, inputState } from './Input'
+import { Radio, radioState } from './Radio'
 
 export const Form = {
+  Checkbox: {
+    Checkbox,
+    checkboxState,
+  },
   Input: {
     Input,
     inputType,
     inputState,
+  },
+  Radio: {
+    Radio,
+    radioState,
   },
 }
