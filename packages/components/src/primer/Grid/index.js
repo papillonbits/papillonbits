4322699ff7fd5ae1f95452cbaf7cfa02fb05a234
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-03-29 14:50:03 +0200

    implement Grid index

---
+++ packages/components/src/primer/Grid/index.js
@@ -0,0 +1,9 @@
+import { DisplayTable } from './DisplayTable'
+import { FlexGrid, flexGridSelection, flexGridState } from './FlexGrid'
+
+export const Grid = {
+  DisplayTable,
+  FlexGrid,
+  flexGridSelection,
+  flexGridState,
+}
