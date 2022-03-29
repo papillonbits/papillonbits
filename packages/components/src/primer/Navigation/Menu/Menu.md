1dc1a5310067b4abfd2fde1d0eb7ad850913dc75
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-03-29 16:42:45 +0200

    implement Navigation Menu component

---
+++ packages/components/src/primer/Navigation/Menu/Menu.md
@@ -0,0 +1,25 @@
+# Menu
+
+> The menu is a vertical list of navigational links. A menu's width and placement must be set by you. If you like, just use our grid columns as a parent. Otherwise, apply a custom width.
+
+# Variants
+
+> - Default, WithIcons, WithHeading
+
+## Default
+
+```jsx
+<Menu ariaAttr={ariaAttr} items={defaultItems} onClick={action('onClick')} />
+```
+
+## WithIcons
+
+```jsx
+<Menu ariaAttr={ariaAttr} items={iconItems} onClick={action('onClick')} />
+```
+
+## WithHeading
+
+```jsx
+<Menu heading={heading} ariaAttr={ariaAttr} items={defaultItems} onClick={action('onClick')} />
+```
---
+++ packages/components/src/primer/Navigation/Menu/__tests__/Menu.int.test.js
@@ -0,0 +1,77 @@
+import { defaultItems as items, custom, regular, withIcons, withHeading } from './Menu.int.story'
+
+jest.mock('@papillonbits/library/array', () => ({
+  getIndexItems: () => {},
+  getIndexItemsWithSelected: () => {},
+}))
+
+jest.mock('@papillonbits/library/hooks', () => ({
+  useState: () => {},
+}))
+
+const libraryArrayMockObject = require('@papillonbits/library/array')
+const libraryHooksMockObject = require('@papillonbits/library/hooks')
+
+describe('<Menu />', () => {
+  let indexItemsDataObject = [
+    { ...items[0], index: 0 },
+    { ...items[1], index: 1 },
+    { ...items[2], index: 2 },
+    { ...items[3], index: 3 },
+  ]
+
+  const newIndexItemsWithSelectedDataObject = [
+    { ...items[0], index: 0, isSelected: true },
+    { ...items[1], index: 1, isSelected: false },
+    { ...items[2], index: 2, isSelected: false },
+    { ...items[3], index: 3, isSelected: false },
+  ]
+
+  const onClickMockFn = jest.fn()
+
+  const setIndexItemsMockFn = jest.fn((indexItems) => {
+    indexItemsDataObject = indexItems
+  })
+
+  beforeEach(() => {
+    jest.spyOn(libraryArrayMockObject, 'getIndexItems').mockReturnValue(indexItemsDataObject)
+
+    jest.spyOn(libraryArrayMockObject, 'getIndexItemsWithSelected').mockReturnValue(newIndexItemsWithSelectedDataObject)
+
+    jest.spyOn(libraryHooksMockObject, 'useState').mockImplementation(() => [indexItemsDataObject, setIndexItemsMockFn])
+  })
+
+  afterEach(() => jest.clearAllMocks())
+
+  describe('Event', () => {
+    test('must return new items without index when onClick()', () => {
+      const mountCustomRender = global.renderMount(custom(onClickMockFn))
+      mountCustomRender.find('nav').first().find('a').first().simulate('click')
+      expect(mountCustomRender.props().onClick).toBe(onClickMockFn)
+      expect(mountCustomRender.props().onClick).toHaveBeenCalledWith(newIndexItemsWithSelectedDataObject)
+    })
+  })
+
+  describe('Render', () => {
+    test('must match regular()', () => {
+      expect(global.renderToJSON(regular())).toMatchSnapshot()
+    })
+
+    test('must match withIcons()', () => {
+      expect(global.renderToJSON(withIcons())).toMatchSnapshot()
+    })
+
+    test('must match withHeading()', () => {
+      expect(global.renderToJSON(withHeading())).toMatchSnapshot()
+    })
+  })
+
+  describe('State', () => {
+    test('must reset new items with index when onClick()', () => {
+      const mountCustomRender = global.renderMount(custom(onClickMockFn))
+      mountCustomRender.find('nav').first().find('a').first().simulate('click')
+      expect(setIndexItemsMockFn).toHaveBeenCalledWith(newIndexItemsWithSelectedDataObject)
+      expect(indexItemsDataObject).toBe(newIndexItemsWithSelectedDataObject)
+    })
+  })
+})
