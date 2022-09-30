f54d5ef1cefb575b99e83e7873f8cbdfbef92ed0
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-09-30 15:02:58 +0200

    conditionally require axe core react

--- packages/library/src/a11y/index.js
+++ packages/library/src/a11y/index.js
@@ -1,7 +1,11 @@
-import axe from '@axe-core/react'
+// https://github.com/dequelabs/axe-core-npm
+// https://www.npmjs.com/package/@axe-core/react
+
+/* eslint-disable global-require */
 
 export function checkAccessibilityIssues(react, reactDOM, delay) {
   if (process.env.NODE_ENV !== 'production') {
+    const axe = require('@axe-core/react')
     axe(react, reactDOM, delay)
   }
 }
