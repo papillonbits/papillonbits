cb9b55c41911b7f1774e6c26f16e9322dbeb9cfc
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-07 21:26:47 +0200

    replace getStorybookMainSetup reference

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const getStorybookMainSetup = require('../packages/library/src/storybook').getStorybookMainSetup
+const getStorybookMainSetup = require('@papillonbits/library/storybook').getStorybookMainSetup
 
 const storiesBasePath = path.resolve(__dirname, '../../papillonbits/packages/components/src/primer/**/__tests__/*.int.story.@(js|mdx)')
 const includeBasePath = path.resolve(__dirname, '../packages/components/src/primer/**/__tests__/*')
