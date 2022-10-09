e1f7befcec206f0e56ab0af55f4a77f7870799e9
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-09 11:44:35 +0200

    merge with webpack common setup

--- packages/library/src/webpack/setup/developmentSetup.js
+++ packages/library/src/webpack/setup/developmentSetup.js
@@ -1,3 +1,4 @@
+import { merge } from 'webpack-merge'
 import { getCSSLoaderStandardSetup } from '../loader/cssLoader'
 import { getPostCSSLoaderStandardSetup } from '../loader/postCSSLoader'
 import { getSassLoaderStandardSetup } from '../loader/sassLoader'
@@ -6,33 +7,37 @@ import { getHotModuleReplacementPluginStandardSetup } from '../plugin/hotModuleR
 import { getDevServerStandardSetup } from '../server/devServer'
 
 export function getWebpackDevelopmentSetup({
+  webpackCommonSetup,
   environmentVariablesFilePath,
   micrositeUrlPath,
   micrositeFolderPaths,
   includedSourceFilePaths,
 }) {
-  return {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: getDevServerStandardSetup(),
-    output: { path: micrositeFolderPaths.build, publicPath: micrositeUrlPath, filename: '[name].bundle.js' },
-    optimization: {
-      concatenateModules: true,
+  return merge([
+    webpackCommonSetup,
+    {
+      mode: 'development',
+      devtool: 'inline-source-map',
+      devServer: getDevServerStandardSetup(),
+      output: { path: micrositeFolderPaths.build, publicPath: micrositeUrlPath, filename: '[name].bundle.js' },
+      optimization: {
+        concatenateModules: true,
+      },
+      plugins: [getDotenvWebpackStandardSetup({ path: environmentVariablesFilePath }), getHotModuleReplacementPluginStandardSetup()],
+      module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            include: includedSourceFilePaths,
+            use: [
+              'style-loader',
+              getCSSLoaderStandardSetup({ sourceMap: false, modules: { localIdentName: '[name]_[local]_[hash:base64:5]' } }),
+              getPostCSSLoaderStandardSetup(),
+              getSassLoaderStandardSetup({ includedSourceFilePaths }),
+            ],
+          },
+        ],
+      },
     },
-    plugins: [getDotenvWebpackStandardSetup({ path: environmentVariablesFilePath }), getHotModuleReplacementPluginStandardSetup()],
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          include: includedSourceFilePaths,
-          use: [
-            'style-loader',
-            getCSSLoaderStandardSetup({ sourceMap: false, modules: { localIdentName: '[name]_[local]_[hash:base64:5]' } }),
-            getPostCSSLoaderStandardSetup(),
-            getSassLoaderStandardSetup({ includedSourceFilePaths }),
-          ],
-        },
-      ],
-    },
-  }
+  ])
 }
--- packages/library/src/webpack/setup/productionSetup.js
+++ packages/library/src/webpack/setup/productionSetup.js
@@ -1,3 +1,4 @@
+import { merge } from 'webpack-merge'
 import { getCSSLoaderStandardSetup } from '../loader/cssLoader'
 import { getBannerPluginStandardSetup } from '../plugin/bannerPlugin'
 import { getCSSMinimizerWebpackPluginStandardSetup } from '../plugin/cssMinimizerWebpackPlugin'
@@ -5,43 +6,52 @@ import { getDotenvWebpackStandardSetup } from '../plugin/dotenvWebpack'
 import { MiniCSSExtractPluginLoader, getMiniCSSExtractPluginAdvancedSetup } from '../plugin/miniCSSExtractPlugin'
 import { getWebpackBundleAnalyzerStandardSetup } from '../plugin/webpackBundleAnalyzer'
 
-export function getWebpackProductionSetup({ environmentVariablesFilePath, micrositeUrlPath, micrositeFolderPaths, packageJSON }) {
-  return {
-    mode: 'production',
-    devtool: false,
-    output: {
-      path: micrositeFolderPaths.build,
-      publicPath: micrositeUrlPath,
-      filename: 'js/[name].[contenthash].bundle.js',
-    },
-    performance: {
-      hints: false,
-      maxEntrypointSize: 512000,
-      maxAssetSize: 512000,
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [getCSSMinimizerWebpackPluginStandardSetup(), '...'],
-      runtimeChunk: { name: 'runtime' },
-    },
-    plugins: [
-      getBannerPluginStandardSetup(packageJSON),
-      getDotenvWebpackStandardSetup({ path: environmentVariablesFilePath }),
-      getMiniCSSExtractPluginAdvancedSetup(),
-      getWebpackBundleAnalyzerStandardSetup(),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.(sass|scss|css)$/,
-          use: [
-            MiniCSSExtractPluginLoader,
-            getCSSLoaderStandardSetup({ sourceMap: false, modules: { localIdentName: '[name]_[local]_[hash:base64:5]' } }),
-            'postcss-loader',
-            'sass-loader',
-          ],
-        },
+export function getWebpackProductionSetup({
+  webpackCommonSetup,
+  environmentVariablesFilePath,
+  micrositeUrlPath,
+  micrositeFolderPaths,
+  packageJSON,
+}) {
+  return merge([
+    webpackCommonSetup,
+    {
+      mode: 'production',
+      devtool: false,
+      output: {
+        path: micrositeFolderPaths.build,
+        publicPath: micrositeUrlPath,
+        filename: 'js/[name].[contenthash].bundle.js',
+      },
+      performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+      },
+      optimization: {
+        minimize: true,
+        minimizer: [getCSSMinimizerWebpackPluginStandardSetup(), '...'],
+        runtimeChunk: { name: 'runtime' },
+      },
+      plugins: [
+        getBannerPluginStandardSetup(packageJSON),
+        getDotenvWebpackStandardSetup({ path: environmentVariablesFilePath }),
+        getMiniCSSExtractPluginAdvancedSetup(),
+        getWebpackBundleAnalyzerStandardSetup(),
       ],
+      module: {
+        rules: [
+          {
+            test: /\.(sass|scss|css)$/,
+            use: [
+              MiniCSSExtractPluginLoader,
+              getCSSLoaderStandardSetup({ sourceMap: false, modules: { localIdentName: '[name]_[local]_[hash:base64:5]' } }),
+              'postcss-loader',
+              'sass-loader',
+            ],
+          },
+        ],
+      },
     },
-  }
+  ])
 }
