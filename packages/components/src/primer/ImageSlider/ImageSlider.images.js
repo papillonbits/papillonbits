69c9de3e74c61fa0b2e208b07c83fb5b4c534586
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2023-08-11 01:50:06 +0200

    add ImageSlider images

---
+++ packages/components/src/primer/ImageSlider/ImageSlider.images.js
@@ -0,0 +1,35 @@
+import image1 from './images/0.jpg'
+import image2 from './images/1.jpg'
+import image3 from './images/2.jpg'
+import image4 from './images/3.jpg'
+
+export const images = [
+  {
+    url: image1,
+    left: 0,
+    top: 0,
+    width: 750,
+    height: 400,
+  },
+  {
+    url: image2,
+    left: 750,
+    top: 25,
+    width: 750,
+    height: 350,
+  },
+  {
+    url: image3,
+    left: 1550,
+    top: 0,
+    width: 750,
+    height: 400,
+  },
+  {
+    url: image4,
+    left: 2500,
+    top: 0,
+    width: 350,
+    height: 400,
+  },
+]
